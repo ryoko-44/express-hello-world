1ea2766cb55b086f79561a5d26232cca16a4f7c4
ryoko-44 <23fi027@ms.dendai.ac.jp>
2025-07-11 16:50:21 +0900

    app

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ expressWs(app)
 
 const port = process.env.PORT || 3001
 let connects = []
+//入室しているユーザー管理(重複を許さない)(カワグチ)
+let players = new Set()
 
 app.use(express.static('public'))
 
@@ -13,7 +15,45 @@ app.ws('/ws', (ws, req) => {
   connects.push(ws)
 
   ws.on('message', (message) => {
+    //メッセージJSONに変換(カワグチ)
+    const msg = JSON.parse(message)
     console.log('Received:', message)
+
+    //参加したら(カワグチ)
+    if (msg.type === 'join') {
+      players.add(msg.id)
+
+      // 全クライアントに現在の参加者リストを送信(カワグチ)
+      const playersMsg = JSON.stringify({
+        type: 'players',
+        players: Array.from(players),
+      })
+
+      connects.forEach((socket) => {
+        if (socket.readyState === 1) {
+          socket.send(playersMsg)
+        }
+      })
+
+      // 他のクライアントに入室通知も送る(カワグチ)
+      const joinMsg = JSON.stringify({ type: 'join', id: msg.id })
+      connects.forEach((socket) => {
+        if (socket.readyState === 1) {
+          socket.send(joinMsg)
+        }
+      })
+      return
+    }
+
+    if (msg.type === 'start') {
+      // 全接続にゲーム開始通知を送る(カワグチ)
+      connects.forEach((socket) => {
+        if (socket.readyState === 1) {
+          socket.send(JSON.stringify({ type: 'start' }))
+        }
+      });
+      return;
+    }
 
     connects.forEach((socket) => {
       if (socket.readyState === 1) {
